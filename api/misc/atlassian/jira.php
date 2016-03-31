40ccbf89a49a3126fbf54ff196dc18fbe9406894
Kirill Berezin <enelar@exsul.net>
2016-03-31 10:28:54 -0400

    Move out jira api request methods

--- api/misc/atlassian/jira.php
+++ api/misc/atlassian/jira.php
@@ -27,19 +27,6 @@ class jira extends api
     $this->dig_data($data);
   }
 
-  private function request_jira($url)
-  {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_USERPWD, conf()->misc->atlassian->jira->apikey);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $res = curl_exec($ch);
-
-    curl_close($ch);
-    return json_decode($res, true);
-  }
-
   private function send($from, $to, $message, $attach = null)
   {
     phoxy::Load("misc/atlassian/jira/footboy")->send($from, $to, $message, $attach);
@@ -155,57 +142,14 @@ class jira extends api
 
   private function construct_nice_user($data)
   {
-    return
-    [
-      "id" => $data['key'],
-      "avatar" => $data['avatarUrls']['48x48'],
-      "title" => $data['displayName'],
-      "url" => $data['self'],
-    ];
+    return phoxy::Load("misc/atlassian/jira/request")
+      ->construct_nice_user($data);
   }
 
   private function construct_nice_issue($data)
   {
-    $fields = $data['issue']['fields'];
-
-    $ret =
-    [
-      "id" => $data['issue']['key'],
-      "url" => $data['issue']['self'],
-      "idmarkdown" => "<https://pingdelivery.atlassian.net/browse/{$data['issue']['key']}|{$data['issue']['key']}>",
-      "title" => $fields['summary'],
-      "type" => $fields['issuetype']['name'],
-      "typeicon" => $fields['issuetype']['iconUrl'],
-      "priority" => $fields['priority']['name'],
-      "priotityicon" => $fields['priority']['iconUrl'],
-      "links" => [],
-      "watches" => [],
-      "members" => [],
-    ];
-
-    $watches = $this->request_jira($fields['watches']['self']);
-
-    foreach ($watches["watchers"] as $watcher)
-      $ret['watches'][] = $this->construct_nice_user($watcher);
-
-    if (!is_array($fields["customfield_10218"]))
-    {
-      if ($fields["customfield_10218"] != null)
-        $this->debuglog($fields["customfield_10218"]);
-    }
-    else
-      foreach ($fields["customfield_10218"] as $member)
-        $ret['members'][] = $this->construct_nice_user($member);
-
-    foreach ($fields['issuelinks'] as $link)
-    {
-      $ret['links'][] =
-      [
-        "linkurl" => $link['self'],
-      ];
-    }
-
-    return $ret;
+    return phoxy::Load("misc/atlassian/jira/request")
+      ->construct_nice_issue($data);
   }
 
   protected function test()
--- api/misc/atlassian/jira/request.php
+++ api/misc/atlassian/jira/request.php
@@ -2,5 +2,71 @@
 
 class request extends api
 {
+  public function construct_nice_user($data)
+  {
+    return
+    [
+      "id" => $data['key'],
+      "avatar" => $data['avatarUrls']['48x48'],
+      "title" => $data['displayName'],
+      "url" => $data['self'],
+    ];
+  }
 
+  public function construct_nice_issue($data)
+  {
+    $fields = $data['issue']['fields'];
+
+    $ret =
+    [
+      "id" => $data['issue']['key'],
+      "url" => $data['issue']['self'],
+      "idmarkdown" => "<https://pingdelivery.atlassian.net/browse/{$data['issue']['key']}|{$data['issue']['key']}>",
+      "title" => $fields['summary'],
+      "type" => $fields['issuetype']['name'],
+      "typeicon" => $fields['issuetype']['iconUrl'],
+      "priority" => $fields['priority']['name'],
+      "priotityicon" => $fields['priority']['iconUrl'],
+      "links" => [],
+      "watches" => [],
+      "members" => [],
+    ];
+
+    $watches = $this->request_jira($fields['watches']['self']);
+
+    foreach ($watches["watchers"] as $watcher)
+      $ret['watches'][] = $this->construct_nice_user($watcher);
+
+    if (!is_array($fields["customfield_10218"]))
+    {
+      if ($fields["customfield_10218"] != null)
+        $this->debuglog($fields["customfield_10218"]);
+    }
+    else
+      foreach ($fields["customfield_10218"] as $member)
+        $ret['members'][] = $this->construct_nice_user($member);
+
+    foreach ($fields['issuelinks'] as $link)
+    {
+      $ret['links'][] =
+      [
+        "linkurl" => $link['self'],
+      ];
+    }
+
+    return $ret;
+  }
+
+  private function request_jira($url)
+  {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_USERPWD, conf()->misc->atlassian->jira->apikey);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    $res = curl_exec($ch);
+
+    curl_close($ch);
+    return json_decode($res, true);
+  }
 }
